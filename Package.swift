822e69486fc58a1bde1464dd7044128caac6d9b5
Guillermo Moraleda <guillermo.moraleda@audi.de>
2024-06-30 10:09:46 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -1,22 +1,33 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "MyBuildToolPlugin",
+    name: "ColorGenerator",
+    platforms: [
+        .iOS(.v16),
+        .watchOS(.v9),
+        .macOS(.v13)
+    ],
     products: [
         // Products can be used to vend plugins, making them visible to other packages.
         .plugin(
-            name: "MyBuildToolPlugin",
-            targets: ["MyBuildToolPlugin"]),
+            name: "ColorGeneratorPlugin",
+            targets: ["ColorGeneratorPlugin"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .plugin(
-            name: "MyBuildToolPlugin",
-            capability: .buildTool()
+            name: "ColorGeneratorPlugin",
+            capability: .buildTool(),
+            dependencies: ["ColorGeneratorExec"]
         ),
+        .executableTarget(
+            name: "ColorGeneratorExec",
+            path: "Sources/ColorGeneratorExec"
+        )
     ]
 )
---
+++ Plugins/ColorGeneratorPlugin/plugin.swift
@@ -0,0 +1,71 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct ColorGenerator: BuildToolPlugin {
+    /// Entry point for creating build commands for targets in Swift packages.
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        guard let target = target as? SourceModuleTarget else {
+            return []
+        }
+
+        let assetCatalogPath = target.sourceFiles(withSuffix: "xcassets")
+            .compactMap { assetCatalog in
+                assetCatalog.path
+            }
+            .first
+
+        if let assetCatalogPath {
+            let outputPath = assetCatalogPath.removingLastComponent().appending("Colors.swift")
+            let invocation = PluginInvocation(catalogPath: assetCatalogPath.string, outputPath: outputPath.string)
+
+            return try [
+                .buildCommand(
+                    displayName: "Generate Color Constants",
+                    executable: context.tool(named: "ColorGeneratorExec").path,
+                    arguments: [invocation.encodedString()],
+                    inputFiles: [],
+                    outputFiles: []
+                )
+            ]
+        } else {
+            return []
+        }
+    }
+}
+
+struct PluginInvocation: Codable {
+    let catalogPath: String
+    let outputPath: String
+
+    func encodedString() throws -> String {
+        let data = try JSONEncoder().encode(self)
+        return String(decoding: data, as: UTF8.self)
+    }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension ColorGenerator: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target _: XcodeTarget) throws -> [PackagePlugin.Command] {
+        let assetCatalogPath = context.xcodeProject.filePaths
+            .filter { $0.string.hasSuffix("xcassets") }
+            .map { $0.string }
+            .first!
+
+        let outputPath = context.pluginWorkDirectory.appending(["Colors.swift"])
+
+        let invocation = PluginInvocation(catalogPath: assetCatalogPath, outputPath: outputPath.string)
+
+        return try [
+            .buildCommand(
+                displayName: "Generate Color Constants",
+                executable: context.tool(named: "ColorGeneratorExec").path,
+                arguments: [invocation.encodedString()],
+                outputFiles: [outputPath]
+            )
+        ]
+    }
+}
+#endif
